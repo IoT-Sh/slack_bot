df7c4d57d4ce4f526300d3d01238b308b8f1ddf7
Shariful Alam <archlinux@ArchMachine.localdomain>
2022-03-30 23:12:07 +0600

    schedule added

--- bot.py
+++ bot.py
@@ -1,27 +1,32 @@
 import requests
 import json
+import schedule
+import time
 import secrets
 from token_generation import accessToken
 
-last_commit = 0
-access_token = accessToken()
+author = []
 
 def send_message(req_count):
+
     header = 'Content-type: application/json'
 
-    msg = "someone requesting to pull"
-    data = '{"text":'+'"'+msg+'"'+'}'
-    url = secrets.bot_url
+    for i in range(len(req_count)):
+        msg = req_count[i]+" has pull request"
+        
+        data = '{"text":'+'"'+msg+'"'+'}'
+        print(data)
+        url = secrets.bot_url
+        print(url)
 
-    response = requests.post(url,data=data)
-    last_commit = get_request()
-    print(response.text)
-    return last_commit
+        response = requests.post(url,data=data)
+        print(response.text)
 
 
 
 def get_request():
     url = secrets.bitbucket_url
+    access_token = accessToken()
 
     headers = {
         "Accept": "application/json",
@@ -29,18 +34,24 @@ def get_request():
     }
     response = requests.get(url,headers=headers)
     y = json.loads(response.text)
-    request_count = (y["size"])
 
-    return request_count
+    
 
+    request_count = (y["values"])
+    for i in range(len(request_count)):
+        s = (request_count[i]["author"]["display_name"])
+        author.append(s)
 
+    return author
+
+def main():
+    request_count = get_request()
+    send_message(request_count)
 
 if __name__ == '__main__':
-    while True:
-        request_count = get_request()
-        
-        if(request_count>last_commit):
-            last_commit = send_message(request_count)
 
-        else:
-            print("no request found")
+    schedule.every().day.at("09:00").do(main)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
